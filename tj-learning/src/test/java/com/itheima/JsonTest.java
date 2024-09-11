340dc77e97f0fe58f41d5bd15026942e95b89d05
SEU-Sefy <89718229+NEFU-Chaofan@users.noreply.github.com>
2024-09-11 18:09:46 +0800

    1:引入dubbo作为RPC调用框架进行服务间调用 2.引入rocketMq作为消息中间件 3.引入sentinel作为限流、熔断降级组件

--- tj-learning/src/test/java/com/itheima/JsonTest.java
+++ tj-learning/src/test/java/com/itheima/JsonTest.java
@@ -10,20 +10,5 @@ import java.util.List;
 
 public class JsonTest {
 
-    @Test
-    public void test() {
-        List<LikedTimesDTO> list = new ArrayList<>();
-        list.add(LikedTimesDTO.of(1782977388850360321L, 1));
-        list.add(LikedTimesDTO.of(17850360321L, 222));
-        LikedTimesMessage msg = new LikedTimesMessage();
-        msg.setBizType("QA");
-        msg.setLikedTimesDTOList(list);
-        String jsonStr = JsonUtils.toJsonStr(msg);
-        System.out.println("jsonStr = " + jsonStr);
-        LikedTimesMessage bean = JsonUtils.toBean(jsonStr, LikedTimesMessage.class);
-        System.out.println("bean = " + bean);
-        String bizType = bean.getBizType();
-        List<LikedTimesDTO> list1 = bean.getLikedTimesDTOList();
-        System.out.println("list1 = " + list1);
-    }
+
 }
